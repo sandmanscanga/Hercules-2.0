3650a925cfc5a4fcdb28f6e063486666ed57e668
Adam Scanga <sandmanscanga@gmail.com>
2022-03-09 20:23:32 -0500

    Added threading capabilities

--- hercules.py
+++ hercules.py
@@ -1,9 +1,14 @@
 """Module for running the Hercules attack script"""
 from parse.main import get_browser
+from threader.main import Threader
 
 
 def main(browser):
     browser.display_final()
+    total_threads = browser.threads
+    attack_function = browser.attack_function
+    result_function = browser.handle_result
+    threader = Threader(total_threads, attack_function, result_function)
 
 
 if __name__ == "__main__":
